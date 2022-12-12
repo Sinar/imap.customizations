0c211a345f9459e3d527156a9465e235ada59ceb
Khairil Yusof <khairil.yusof@gmail.com>
2022-12-12 13:15:41 +0900

    Initial example of tal repeat for News

--- src/imap/customizations/views/front_page.py
+++ src/imap/customizations/views/front_page.py
@@ -3,6 +3,7 @@
 # from imap.customizations import _
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
+from plone import api
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -15,6 +16,18 @@ class FrontPage(BrowserView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('front_page.pt')
+
+    def latest_news(self):
+        news = api.content.find(portal_type='News Item')
+        return news
+
+    def latest_activities(self):
+        news = api.content.find(portal_type='Activity')
+        return news
+
+    def latest_resources(self):
+        news = api.content.find(portal_type='Resource')
+        return news
 
     def __call__(self):
         # Implement your own actions:
