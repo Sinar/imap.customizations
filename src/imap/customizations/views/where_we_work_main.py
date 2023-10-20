5e73e7bb33f3955e8a1661ae6dc459ef481e1e4c
sitinurliza <siti.nurliza@sinarproject.org>
2023-10-20 13:18:42 +0800

    "Add view: where-we-work-main"

---
+++ src/imap/customizations/views/where_we_work_main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# from imap.customizations import _
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class IWhereWeWorkMain(Interface):
+    """ Marker Interface for IWhereWeWorkMain"""
+
+
+@implementer(IWhereWeWorkMain)
+class WhereWeWorkMain(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('where_we_work_main.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
